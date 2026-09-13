bd51539fc9861d967c4e23385506fbc50c03dc9f
Marvelous Ekhator <grovegoals@gmail.com>
2026-09-13 12:46:56 +0100

    Create coderoom-sync-server/server.js

---
+++ coderoom-sync-server/server.js
@@ -0,0 +1,14 @@
+// coderoom-sync-server/server.js
+const http = require("http");
+const { WebSocketServer } = require("ws");
+const { setupWSConnection } = require("y-websocket/bin/utils");
+
+const server = http.createServer();
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (conn, req) => {
+  setupWSConnection(conn, req);
+});
+
+const PORT = process.env.PORT || 1234;
+server.listen(PORT, () => console.log(`Sync server running on ${PORT}`));
