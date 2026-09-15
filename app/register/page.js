29f4e5e8da885bb5509b2470fea289e78fbdcd14
Marvelous Ekhator <grovegoals@gmail.com>
2026-09-15 23:47:07 +0100

    Update page.js

--- app/register/page.js
+++ app/register/page.js
@@ -1,7 +1,7 @@
 "use client";
 import BirthdayPicker from "@/components/BirthdayPicker";
-import { useState, useEffect } from "react";import { useRouter } from "next/
-navigation";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { Mail, Lock, User, Eye, EyeOff, Loader2, AlertCircle, ChevronLeft, Calendar } from "lucide-react";
 import { PasswordRequirement, UsernameStatus } from "@/components/AuthWidgets";
