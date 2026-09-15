d3b4efba8d75e7967c127f565eb08fa4681d43c5
Marvelous Ekhator <grovegoals@gmail.com>
2026-09-15 21:50:24 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,19 @@ const nextConfig = {
       static: 0,
     },
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
