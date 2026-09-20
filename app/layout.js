2cbc035e6ba643472d127d4cda57f672578469a3
Marvelous Ekhator <grovegoals@gmail.com>
2026-09-20 16:47:51 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Outfit, Inter } from "next/font/google";
 import "./globals.css";
+import Tracker from "../components/Tracker";
 
 const outfit = Outfit({ subsets: ["latin"], weight: ["500", "600", "700"], variable: "--font-display" });
 const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "600"], variable: "--font-body" });
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
       <body className={`${outfit.variable} ${inter.variable}`} style={{ fontFamily: "var(--font-body)" }}>
+        <Tracker />
         {children}
       </body>
     </html>
